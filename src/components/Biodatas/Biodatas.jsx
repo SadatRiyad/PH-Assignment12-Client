284fe584997ed969dc6cdd31e8a75af28e016254
SadatRiyad <sadatriyad@students.diu.ac>
2024-06-21 00:21:05 +0600

    Biodatas page search implement

--- src/components/Biodatas/Biodatas.jsx
+++ src/components/Biodatas/Biodatas.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import useBiodatas from '../Hooks/useBiodatas/useBiodatas';
 import ProfileCard from '../Home/PremiumBanner/ProfileCard/ProfileCard';
 import { Button } from '../ui/button';
+import { Search } from 'lucide-react';
 
 const Biodatas = () => {
     const [biodatas, , loading] = useBiodatas();
@@ -10,10 +11,29 @@ const Biodatas = () => {
     const [ageRange1, setAgeRange1] = useState([18, 99]);
     const [selectedType, setSelectedType] = useState('');
     const [selectedDivision, setSelectedDivision] = useState('');
-    // console.log(biodatas)
+    const [filterBiodatas, setFilterBiodatas] = useState([]);
+
+    useEffect(()=>{
+        const filteredBiodatas = biodatas.filter(biodata => {
+            return (
+                (selectedType ? biodata.biodataType === selectedType : true) &&
+                (selectedDivision ? biodata.presentDivision === selectedDivision : true) &&
+                (biodata.age >= ageRange[0] && biodata.age <= ageRange[1] || biodata.age >= ageRange && biodata.age <= ageRange1)
+            );
+        });
+        setFilterBiodatas(filteredBiodatas);
+    },[ageRange, ageRange1, biodatas, selectedDivision, selectedType])
 
     if (loading) {
         return <div className="flex text-center items-center justify-center h-dvh w-dvw">Loading...</div>
+    }
+
+    // search with biodataID
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const search = e.target.value;
+        const SearchedBiodatas = biodatas.filter(biodata => biodata.biodataID.toLowerCase().includes(search.toLowerCase()));
+        setFilterBiodatas(SearchedBiodatas);
     }
 
     const handleFilterChange = (e) => {
@@ -28,14 +48,8 @@ const Biodatas = () => {
         }
     };
 
-    const filteredBiodatas = biodatas.filter(biodata => {
-        return (
-            (selectedType ? biodata.biodataType === selectedType : true) &&
-            (selectedDivision ? biodata.presentDivision === selectedDivision : true) &&
-            (biodata.age >= ageRange[0] && biodata.age <= ageRange[1] || biodata.age >= ageRange && biodata.age <= ageRange1)
-        );
-    });
-    console.log(filteredBiodatas)
+
+    // console.log(filterBiodatas);
 
     return (
         <div className="grid container mx-auto px-4 min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
@@ -43,9 +57,9 @@ const Biodatas = () => {
                 <title>Biodatas | BB-Matrimony</title>
             </Helmet>
 
-            <div className="flex h-full max-h-screen flex-col pt-6 gap-2 pr-4">
+            <div className="flex h-full max-h-screen flex-col pt-6 gap-2 md:pr-4">
                 <h2 className="text-4xl font-bold mb-4 mt-4 md:mt-0">Filters</h2>
-                <div className="mb-4 border-2 p-2 px-4">
+                <div className="mb-4 border-2 border-customGulabi p-2 px-4 bg-slate-200">
                     <div className='justify-between flex items-center mb-1'>
                         <label className="block mb-2 font-medium">Age Range: {ageRange[0]} {ageRange1[1] ? '-' : ""} {ageRange[1]}</label>
                         {/* reset */}
@@ -54,7 +68,7 @@ const Biodatas = () => {
                                 setAgeRange([18, 99]);
                                 setAgeRange1([18, 99]);
                             }}
-                            className="bg-red-500 m-0 text-white py-0 rounded-full text-xs"
+                            className="bg-customGulabi m-0 text-white py-0 rounded-full text-xs"
                         >Reset</Button>
                     </div>
                     <input
@@ -72,17 +86,17 @@ const Biodatas = () => {
                         <span>99</span>
                     </div>
                 </div>
-                <div className="mb-4">
+                <div className="mb-4 border-2 p-3 border-customGulabi bg-slate-200">
                     <label className="block mb-2">Biodata Type:</label>
-                    <select name="type" value={selectedType} onChange={handleFilterChange} className="w-full border-2 p-2 rounded-full">
+                    <select name="type" value={selectedType} onChange={handleFilterChange} className="w-full border-2 border-customGulabi  p-2 rounded-full">
                         <option value="">All</option>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
                     </select>
                 </div>
-                <div className="mb-4">
+                <div className="mb-4 border-2 p-3 border-customGulabi bg-slate-200">
                     <label className="block mb-2">Present Division:</label>
-                    <select name="division" value={selectedDivision} onChange={handleFilterChange} className="w-full border-2 p-2 rounded-full">
+                    <select name="division" value={selectedDivision} onChange={handleFilterChange} className="w-full border-2 border-customGulabi p-2 rounded-full">
                         <option value="">All</option>
                         <option value="Dhaka">Dhaka</option>
                         <option value="Chattagram">Chattagram</option>
@@ -95,10 +109,27 @@ const Biodatas = () => {
                 </div>
             </div>
             <div className="w-full h-fit pt-6 md:pl-5 md:border-l-4 min-h-dvh">
-                <h2 className="text-3xl md:text-4xl font-bold mb-6">Total Biodatas: {biodatas.length}</h2>
+                <div className='grid w-full grid-cols-1 md:grid-cols-2 text-center md:text-left'>
+                    <div className='w-full'>
+                        <h2 className="text-3xl font-bold mb-2 md:mb-5">Biodatas Found: <span className='text-customGulabi'>{filterBiodatas.length}</span></h2>
+                    </div>
+                    <div className="w-full flex-1">
+                        <form>
+                            <div className="relative justify-end items-end flex mb-8 md:mb-5">
+                                <Search className="absolute right-3.5 top-[14px] h-4 w-4 text-muted-foreground" />
+                                <input
+                                    type="search"
+                                    placeholder="Search by Biodata ID"
+                                    className="w-full border-2 p-2 rounded-full border-customGulabi  appearance-none bg-background pr-8 pl-4 shadow-none md:w-fit lg:w-2/3"
+                                    onChange={handleSearch}
+                                />
+                            </div>
+                        </form>
+                    </div>
+                </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
                     {/* {filteredBiodatas.slice(0, 20).map(profile => ( */}
-                    {filteredBiodatas.map(profile => (
+                    {filterBiodatas.map(profile => (
                         <ProfileCard key={profile._id} profile={profile} />
                     ))}
                 </div>
